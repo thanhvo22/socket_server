93e81f7490b0cae228697984a87c75dc9f8ab378
Thanh_Vo <thanhvp2707@gmail.com>
2022-05-24 10:57:57 +0700

    update

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -5,7 +5,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/socket");
+    await mongoose.connect("mongodb+srv://thanhvo:2212@test-socket.ot0ez.mongodb.net/?retryWrites=true&w=majority");
     console.log("connecting successfully hhhh");
   } catch (error) {
     console.log("failllllllllll");
